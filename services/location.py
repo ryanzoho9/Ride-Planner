6e2dfb4e061aeee02991281555c4ca1134417b1a
David Chan <davidochan@gmail.com>
2025-01-25 10:38:49 -0800

    readjusting current branch

---
+++ services/location.py
@@ -0,0 +1,12 @@
+import requests
+
+# converts address to coordinates. returns a list of longitude and latitude
+def convert_address(address):
+    coordinates = []
+    if not address:
+        raise Exception("Invalid Address")
+    
+    # call api
+    # coordinates = LOCATION API
+
+    
