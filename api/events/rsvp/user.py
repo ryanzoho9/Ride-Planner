4eb96f2c11bb096a1d7a798af6cdacba100c72fa
David Chan <davidochan@gmail.com>
2025-01-25 11:40:46 -0800

    Initialized WebSocket server with Flask-SocketIO. Configured test client for testing

--- api/events/rsvp/user.py
+++ api/events/rsvp/user.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, jsonify, request
-import uuid
+from services.uuid_generator import get_uuid
 
 user_blueprint = Blueprint('user', __name__, url_prefix='/events/rsvp')
 
 @user_blueprint.route('/create_user', methods=['POST'])
-def create_event():
+def create_user():
     data = request.get_json()
     if not data:
         return jsonify({"error": "Invalid JSON Format"}), 400
@@ -18,5 +18,5 @@ def create_event():
     
     # INSERT INFO INTO DB ONCE INITIALIZED
 
-    user_uuid = str(uuid.uuid4())
+    user_uuid = get_uuid()
     return jsonify({"user_uuid": user_uuid}), 200
