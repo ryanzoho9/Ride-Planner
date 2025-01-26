3ec36237ee8cacfa3b4d809f95300cb0c633839c
AryaGill <75190244+AryaGill@users.noreply.github.com>
2025-01-25 18:49:46 -0800

    Create eventa_all functionality

---
+++ api/events/rsvp/event_userAll.py
@@ -0,0 +1,37 @@
+from flask import Blueprint, jsonify, request
+import os
+import psycopg2
+from dotenv import load_dotenv
+import json
+
+load_dotenv()
+
+
+def event_allUser(event_uuid):
+    db_host = os.getenv("DB_HOST")
+    db_port = os.getenv("DB_PORT")
+    db_user = os.getenv("DB_USER")
+    db_pass = os.getenv("DB_PASS")
+    db_name = os.getenv("DB_NAME")
+
+    try:
+        conn = psycopg2.connect(
+            host=db_host, dbname=db_name, user=db_user, password=db_pass, port=db_port
+        )
+        cur = conn.cursor()
+        cur.execute(
+            """
+                    SELECT * FROM users WHERE event_id = %s
+                    """,
+            (event_uuid,),
+        )
+        colnames = [desc[0] for desc in cur.description]
+        rows = cur.fetchall()
+        result = [dict(zip(colnames, row)) for row in rows]
+        json_result = json.dumps(result, default=str)
+        conn.commit()
+        conn.close()
+        return json_result
+
+    except Exception as e:
+        return {"error": f"Database error: {str(e)}"}
--- database/connect_db.py
+++ database/connect_db.py
@@ -5,6 +5,7 @@ import psycopg2
 
 load_dotenv()
 
+
 def get_db_connection():
     try:
         conn = psycopg2.connect(
@@ -12,28 +13,9 @@ def get_db_connection():
             dbname=os.getenv("DB_NAME"),
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD"),
-            port=os.getenv("DB_PORT")
+            port=os.getenv("DB_PORT"),
         )
         return conn
     except Exception as e:
         print(f"Database connection failed: {e}")
         raise
-    
-    
-def initializeDb(QUERY):
-db_host = os.getenv("DB_HOST")
-db_port = os.getenv("DB_PORT")
-db_user = os.getenv("DB_USER")
-db_pass = os.getenv("DB_PASS")
-db_name = os.getenv("DB_NAME")
-
-try:
-    conn = psycopg2.connect(
-        host=db_host, dbname=db_name, user=db_user, password=db_pass, port=db_port
-    )
-    cur = conn.cursor()
-    cur.execute(QUERY)
-    conn.commit()
-    conn.close()
-except Exception as e:
-    return jsonify({"error": f"Database error: {str(e)}"}), 500
