02dce28ba4d9520445f9b8250592a370662d45fa
AryaGill <75190244+AryaGill@users.noreply.github.com>
2025-01-25 18:15:11 -0800

    Created initializeDB(QUERY)

--- database/connect_db.py
+++ database/connect_db.py
@@ -1,6 +1,7 @@
 import psycopg2
 import os
 from dotenv import load_dotenv
+import psycopg2
 
 load_dotenv()
 
@@ -17,3 +18,22 @@ def get_db_connection():
     except Exception as e:
         print(f"Database connection failed: {e}")
         raise
+    
+    
+def initializeDb(QUERY):
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_user = os.getenv("DB_USER")
+db_pass = os.getenv("DB_PASS")
+db_name = os.getenv("DB_NAME")
+
+try:
+    conn = psycopg2.connect(
+        host=db_host, dbname=db_name, user=db_user, password=db_pass, port=db_port
+    )
+    cur = conn.cursor()
+    cur.execute(QUERY)
+    conn.commit()
+    conn.close()
+except Exception as e:
+    return jsonify({"error": f"Database error: {str(e)}"}), 500
